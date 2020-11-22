30393085d2cda202c27f7deeef31f4b881a5837c
Andrew Gurylev <andrew@gurylev.com>
2020-11-22 21:42:16 +0300

    rename 1password cli methods for compatibility

--- custom/functions/1password.zsh
+++ custom/functions/1password.zsh
@@ -1,36 +1,43 @@
 # 1password - most handy passwords keychain for mac
 # https://support.1password.com/command-line-getting-started/
 
-# Authentification
-# Sessions automatically expire after 30 minutes of inactivity. To manually use 1p-signout
-1p-signin() {
-  eval $(op signin my)
+# First authorize with key!
+# 
+# op signin <signinaddress> <emailaddress> <secretkey>
+# 
+# Example account details:
+# <signinaddress>   example.1password.com
+# <emailaddress>    user@example.com
+# <secretkey>       A3-XXXXXX-XXXXXX-XXXXX-XXXXX-XXXXX-XXXXX
+
+export OPSUBDOMAIN="my"
+
+# Login snippet (session vs raw - see help: `op signin --help`)
+local _op-signin() {
+  op signin $OPSUBDOMAIN --output=raw
 }
 
-# Logout
-1p-signout() {
-  op signout
-}
+# Logout: op signout
 
 # Get login of passed item
 # @param (string) - UUID or name of site in 1password
 # @see https://support.1password.com/command-line/#get-details
-1p-get-login() {
-  op get item $1 | jq -r '.details.fields[] | select(.designation=="username").value'
+op-get-login() {
+  _op-signin | op get item $1 | jq -r '.details.fields[] | select(.designation=="username").value'
 }
 
 # Get password of passed item
 # @param (string) - UUID or name of site in 1password
 # @see https://support.1password.com/command-line/#get-details
-1p-get-pass() {
-  op get item $1 | jq -r '.details.fields[] | select(.designation=="password").value'
+op-get-pass() {
+  _op-signin | op get item $1 | jq -r '.details.fields[] | select(.designation=="password").value'
 }
 
 # Get totp of passed item
 # @param (string) - UUID or name of site in 1password
 # @see https://support.1password.com/command-line/#get-details
-1p-get-totp() {
-  op get totp $1
+op-get-totp() {
+  _op-signin | op get totp $1
 }
 
 # Get login of passed item
