89689d81a65c864d9385c306828537affa61f7b5
Andrew Gurylev <andrew@gurylev.com>
2021-09-09 00:55:11 +0300

    add Brewfile; close #2

--- brew.sh
+++ brew.sh
@@ -24,44 +24,7 @@ if [[ $SHELL != "/bin/zsh" ]]; then
 	chsh -s $(which zsh)
 fi
 
-# Security
-brew cask install 1password 1password-cli gpg2
-brew cask install little-snitch micro-snitch
-brew cask install protonvpn tunnelblick openconnect veracrypt
-
-# For CLI
-brew install homebrew/cask-fonts/font-hack-nerd-font
-brew install mas-cli/tap/mas
-mas install 497799835 # XCode
-
-# For manager
-mas install 1176895641 # Spark (mail)
-brew cask install appcleaner omnidisksweeper
-brew cask install notion typora
-brew cask install slack telegram
-brew cask install flux
-
-# Finder extenion
-# https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlimagesize webpquicklook qlstephen qlmarkdown quicklook-json betterzip suspicious-package
-
-# Restart quicklook
-defaults write org.n8gray.QLColorCode extraHLFlags '-l'
-qlmanage -r
-qlmanage -m
-
-# For developer
-brew install iterm2 visual-studio-code sublime-text phpstorm
-brew install wget composer openssl nginx php71 mysql
-brew install imagemagick graphicsmagick
-brew install wget Schniz/tap/fnm bat ripgrep git-standup lsd jq git-delta
-brew cask install figma imagealpha imageoptim svgo
-
-# Video
-brew cask install iina vlc ffmpeg
-
-# with configure after install
-brew install fzf
+brew bundle install
 
 $(brew --prefix)/opt/fzf/install
 
--- zsh/aliases/brew.zsh
+++ zsh/aliases/brew.zsh
@@ -2,3 +2,5 @@
 alias brs="brew services"
 alias bri="brew install"
 alias brl="brew list -1"
+alias update="brew cu"
+# Advanced tips: https://gist.github.com/ChristopherA/a579274536aab36ea9966f301ff14f3f#advanced-topics--to-investigate
