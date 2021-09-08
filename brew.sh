dca3d01f323a71afffbd5aa651f699c278119807
Andrew Gurylev <andrew@gurylev.com>
2021-09-09 01:09:30 +0300

    install brew via bash, not ruby

--- brew.sh
+++ brew.sh
@@ -2,7 +2,7 @@
 
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Hombrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Check your system for potential problems
