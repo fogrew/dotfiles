53c8754d2e6b68476ad2d21f5b4fbd116a8f57ef
Andrew Gurylev <andrew@gurylev.com>
2022-11-24 18:06:50 +0200

    chore: disable docker in prompt

--- zsh/vars/spaceship-theme.zsh
+++ zsh/vars/spaceship-theme.zsh
@@ -4,4 +4,4 @@ SPACESHIP_PROMPT_SEPARATE_LINE=false
 SPACESHIP_PROMPT_PREFIXES_SHOW=false
 SPACESHIP_TIME_SHOW=true
 SPACESHIP_BATTERY_THRESHOLD=30
-SPACESHIP_PROMPT_ORDER=(time dir git package golang node php docker exit_code line_sep char)
+SPACESHIP_PROMPT_ORDER=(time dir git package golang node php exit_code line_sep char)
