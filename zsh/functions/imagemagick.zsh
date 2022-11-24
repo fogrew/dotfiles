494b6f3c5b289802c04c866f11d95582d670f2a6
Andrew Gurylev <andrew@gurylev.com>
2022-11-24 18:06:25 +0200

    feat: add conver.all for imagick this method will conver all raster graphics in destination

--- zsh/functions/imagemagick.zsh
+++ zsh/functions/imagemagick.zsh
@@ -1,2 +1,8 @@
+# WIP
+function convert.all() {
+  local from=$1
+  local to=$2
+  for i in *.jpg; do convert $i ${i%jpg}png; done
+}
 # show dimensions of each images in dir, requires imagemagick
 alias lsizes="identify -format \"%f: %wx%h\n\" *.{jpg,jpeg,png,gif,webp}"
